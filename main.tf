fe6a463856a04334d945b3cc119a7c29871998c4
tvdv <tedv@live.com>
2022-12-01 10:16:45 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    azurerm = ">= 3.29.0"
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
